551af32083b0737d291f8f4369049e521eb6386e
Tien Le <tienlx@gmail.com>
2014-10-15 13:57:41 +0700

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 EhjobAuthentication::Engine.routes.draw do
-  devise_for :users, skip: [:registrations, :passwords, :sessions] do
-    get 'users/sign_in' => 'sessions#new', as: :new_user_session
-  end
+  devise_for :users, skip: [:registrations, :passwords, :sessions]
 end
