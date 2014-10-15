373f082362021959771091373c6734864dace7bf
Tien Nguyen <nqtien310@gmail.com>
2014-10-15 17:13:09 +0700

    configurable EhjobAuthentication

---
+++ lib/ehjob_authentication/config.rb
@@ -0,0 +1,10 @@
+module EhjobAuthentication
+  class Config
+    attr_accessor :app, :base_url
+
+    def authenticate_url
+      raise "'base_url' isn't set yet" if base_url.nil?
+      "#{base_url}/api/users/authenticate"
+    end
+  end
+end
---
+++ test/dummy/spec/lib/ehjob_authentication_spec.rb
@@ -0,0 +1,16 @@
+describe EhjobAuthentication do
+  describe '.config' do
+    it 'inits Configs obj' do
+      EhjobAuthentication.configure do |config|
+        config.app = 'EH'
+        config.base_url = 'http://www.employmenthero.com'
+      end
+
+      config = EhjobAuthentication.config
+      expect(config).to be_instance_of(EhjobAuthentication::Config)
+      expect(config.app).to eq 'EH'
+      expect(config.base_url).to eq 'http://www.employmenthero.com'
+      expect(config.authenticate_url).to eq 'http://www.employmenthero.com/api/users/authenticate'
+    end
+  end
+end
