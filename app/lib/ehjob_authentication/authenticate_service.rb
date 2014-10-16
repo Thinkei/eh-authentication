88ce469c40eeca3c641bf272ec5bf7db54a780da
Tien Nguyen <nqtien310@gmail.com>
2014-10-16 13:40:04 +0700

    Update authenticate_service  - Add url to response json  - validates user credentials to be exited before calling the request

--- app/lib/ehjob_authentication/authenticate_service.rb
+++ app/lib/ehjob_authentication/authenticate_service.rb
@@ -3,9 +3,12 @@ require 'httparty'
 module EhjobAuthentication
   class AuthenticateService
     class << self
-      def call(user_params)
-        HTTParty.post(authenticate_url, body: post_body(user_params))
+      def call(params)
+        validate(params)
         service = HTTParty.post(authenticate_url, body: post_body(user_params), headers: post_headers)
+        parse_response(service)
+      rescue
+        {}
       end
 
       def authenticate_url
@@ -23,6 +26,25 @@ module EhjobAuthentication
           'Content-Type' => 'application/json',
           'Accept' => 'application/json'
         }
+      end
+
+      def parse_response(service)
+        response = {}
+
+        if service.success?
+          response = JSON.parse(service.response.body)
+          if response['id'].present? #user exists
+            response.merge({'url' => EhjobAuthentication.config.base_url})
+          end
+        end
+
+        response
+      end
+
+      def validate(params)
+        raise unless (params['user'].present? &&
+           params['user']['email'].present? &&
+           params['user']['password'].present?)
       end
     end
   end
