f15ba59a0a10e44c65500987494e3d55a59ba1b2
Tien Nguyen <nqtien310@gmail.com>
2014-10-17 13:40:29 +0700

    Include #highest_role & #terminated to User

---
+++ app/lib/ehjob_authentication/roleable.rb
@@ -0,0 +1,28 @@
+#All roles
+#+ EH
+#++ owner/employer
+#++ employee => both employee & manager
+
+#+ Job
+#++ hiring_manager
+#++ job_seeker
+
+module EhjobAuthentication
+  module Roleable
+    def highest_role
+      if EhjobAuthentication.config.hr?
+        role = (user.employer_or_owner? ? 'owner/employer' : 'employee')
+      else
+        'job_seeker'
+      end
+    end
+
+    def terminated
+      if EhjobAuthentication.config.hr?
+        memberships.any? && memberships.all? { |m| m.termination_date? }
+      else
+        nil
+      end
+    end
+  end
+end
--- lib/ehjob_authentication/engine.rb
+++ lib/ehjob_authentication/engine.rb
@@ -2,5 +2,11 @@ module EhjobAuthentication
   class Engine < ::Rails::Engine
     isolate_namespace EhjobAuthentication
     config.autoload_paths << File.expand_path("../../../app/lib", __FILE__)
+
+    config.after_initialize do
+      User.instance_eval do
+        include EhjobAuthentication::Roleable
+      end
+    end
   end
 end
