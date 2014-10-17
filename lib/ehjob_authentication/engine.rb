d91ba7cfb9023860950c7da0b359d08e73dac73c
Tien Le <tienlx@gmail.com>
2014-10-17 14:16:09 +0700

    Add assets and locales for login form

--- lib/ehjob_authentication/engine.rb
+++ lib/ehjob_authentication/engine.rb
@@ -1,6 +1,7 @@
 module EhjobAuthentication
   class Engine < ::Rails::Engine
     isolate_namespace EhjobAuthentication
-    config.autoload_paths << File.expand_path("../../../app/lib", __FILE__)
+    config.autoload_paths << config.root.join('app', 'lib')
+    config.i18n.load_path += Dir[config.root.join('config', 'locales', '*.yml').to_s]
   end
 end
