ff010b85c3ef8c09ea3c6041f1ca05c99062c1dd
Tien Le <tienlx@gmail.com>
2014-10-17 15:31:44 +0700

    Tidy up the url extractor service

--- app/lib/ehjob_authentication/url_extractor_service.rb
+++ app/lib/ehjob_authentication/url_extractor_service.rb
@@ -15,15 +15,16 @@
 module EhjobAuthentication
   class UrlExtractorService
     class << self
+      delegate :hr?, :job?, :eh_url, :job_url, to: 'EhjobAuthentication.config'
+
       def call(params, local_user)
         associate_user = ApiClient.instance.associate_user(params)
-        roles = [local_user, associate_user].map {|u| u.try(:highest_role)}
-        roles = roles.compact
+        roles = [local_user, associate_user].compact.map(&:highest_role)
         is_terminated = [local_user, associate_user].compact.any?(&:terminated)
-        if roles == []
-          raise 'not found'
 
-        elsif roles.include?('employee') || roles.include?('owner/employer')
+        raise 'not found' if roles.empty?
+
+        if roles.include?('employee') || roles.include?('owner/employer')
           if is_terminated
             #TODO create job seeker on job
             job_url if hr?
@@ -37,33 +38,6 @@ module EhjobAuthentication
         elsif roles == ['hiring_manager']
           "#{job_url}/jobs" if hr?
         end
-      end
-
-      def call_for_omniauth(params, local_user)
-        params.merge(type: 'omniauth')
-        call(params, local_user)
-      end
-
-      private
-
-      def config
-        EhjobAuthentication.config
-      end
-
-      def hr?
-        config.hr?
-      end
-
-      def job?
-        config.job?
-      end
-
-      def eh_url
-        config.eh_url
-      end
-
-      def job_url
-        config.job_url
       end
     end
   end
